11517eafeb94fc5b48954222946e501d17d7177c
peaceAndCode <luigimastronardo00@gmail.com>
2024-05-19 14:39:28 +0200

    exception handling for validators annotations

--- src/main/java/com/peaceandcode/expensemanager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/peaceandcode/expensemanager/exception/GlobalExceptionHandler.java
@@ -1,14 +1,25 @@
 package com.peaceandcode.expensemanager.exception;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
   private static ResponseEntity<ExceptionObj> handleException(HttpStatus httpStatus, Exception ex){
     ExceptionObj exceptionObj = new ExceptionObj();
 
@@ -35,6 +46,24 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(AuthenticationFailed.class)
   public ResponseEntity<ExceptionObj> handleAuthenticationFailed(AuthenticationFailed ex, WebRequest request){
     return handleException(HttpStatus.UNAUTHORIZED,ex);
+  }
+
+  @Override
+  public ResponseEntity<Object> handleMethodArgumentNotValid(
+    MethodArgumentNotValidException ex,
+    @NonNull HttpHeaders headers,
+    @NonNull HttpStatusCode status,
+    @NonNull WebRequest request
+  ) {
+
+    Map<String,Object> body = new HashMap<>();
+    List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toList();
+
+    body.put("timestamp", new Timestamp(System.currentTimeMillis()));
+    body.put("statusCode",HttpStatus.BAD_REQUEST.value());
+    body.put("errors",errors);
+
+    return new ResponseEntity<>(body,HttpStatus.BAD_REQUEST);
   }
 
 }
