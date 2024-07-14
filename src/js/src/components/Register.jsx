d3e4b45dedd604b6f39bc14041f688c47a697db2
peaceAndCode <luigimastronardo00@gmail.com>
2024-07-14 12:04:13 +0200

    Register page and signu link in signin page

--- src/js/src/components/Register.jsx
+++ src/js/src/components/Register.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
-
+import {Button, TextField, MenuItem, Typography, Grid} from "@mui/material";
+import expenseManagerLogo from "../assets/expensemanager-logo.svg"
+import {Link} from "react-router-dom";
+import {ContainerForm} from "../containers";
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <ContainerForm>
+            <div>
+                <img src={expenseManagerLogo} alt="expense manager logo"/>
+            </div>
+            <Grid container spacing={2} component="form">
+                <Grid item xs={12} md={6}>
+                   <TextField label="First name" variant="filled"/>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <TextField type="password" label="Last name" variant="filled"/>
+                </Grid>
+                <Grid item md={12}>
+                    <TextField label="E-mail" variant="filled"/>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <TextField label="Password" variant="filled"/>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <TextField select label="Currency" defaultValue="EUR"  variant="filled">
+                        <MenuItem key="EUR" value="EUR">€</MenuItem>
+                    </TextField>
+                </Grid>
+            </Grid>
+
+            <div className="signin-link-container">
+                <Typography variant="subtitle1" component="p">
+                    Do you already have an account? <Link to="/signin">Sign in</Link>
+                </Typography>
+            </div>
+
+            <Button variant="contained">
+                Sign up
+            </Button>
+        </ContainerForm>
     );
 };
-
 export default Register;
---
+++ src/js/src/containers/ContainerForm.jsx
@@ -0,0 +1,49 @@
+import {Container} from "@mui/material";
+
+import React from 'react';
+import styled from "styled-components";
+
+const ContainerForm = ({children}) => {
+    return (
+        <ContainerCustom maxWidth="sm">
+            {children}
+        </ContainerCustom>
+    );
+};
+
+const ContainerCustom = styled(Container)`
+    margin-top: 8rem;
+    margin-bottom: 8rem;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    & div img{
+        margin-bottom: 2rem;
+        height: 200px;
+        width: 100%;
+    }
+    & form div .MuiTextField-root{
+        width: 100%;
+    }
+    
+    & form{
+        margin-bottom: 1rem;
+    }
+    
+    & div.signup-link-container{
+        margin-bottom: 1rem;
+    }
+
+    & div.signin-link-container{
+        margin-bottom: 1rem;
+    }
+    
+    & button {
+        width: 100%;
+    }
+`
+
+export default ContainerForm;
